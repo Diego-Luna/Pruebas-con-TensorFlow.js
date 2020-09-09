d3c28c5af8ea8691a655beee1977e9afd114f6fa
Diego Luna <diegofcolunalopez@gmail.com>
2020-09-09 15:49:28 -0500

    cambio de de configuracion

--- script.js
+++ script.js
@@ -1,82 +1,194 @@
-let net;
 
-const imgEl = document.getElementById('img');
-const desEl = document.getElementById('descripcion_img');
-const webcamElement = document.getElementById('webcamp');
-const classiFier = knnClassifier.create();
+let model;
 
-async function app() {
+const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
+
+const imgEl = document.getElementById("img");
+const descEl = document.getElementById("descripcion_imagen");
+var count = 0;
+var net ;
+var webcam;
+async function app(){
+	console.log("Cargando modelo de identificacion de imagenes");
   net = await mobilenet.load();
+	console.log("Carga terminada")
+  //clasificamos la imagen de carga
+	const result = await net.classify(imgEl);
+	console.log(result);
+  descEl.innerHTML= JSON.stringify(result);
 
-  var result = await net.classify(imgEl);
 
-  console.log(result);
-  displayImagePredicction();
 
-  // para usar y ver la camara WebCamp
-  webcam = await tf.data.webcam(webcamElement);
 
+  //obtenemos datos del webcam
+	webcam = await tf.data.webcam(webcamElement);
+  //y los vamos procesando
   while (true) {
     const img = await webcam.capture();
 
     const result = await net.classify(img);
 
-    const activation = net.infer(img, "conv_preds");
+    const activation = net.infer(img, 'conv_preds');
     var result2;
-
     try {
-      result2 = await classiFier.predicClass(activation);
-      const classes = ['Untrained', 'Gato', 'Dino', 'Diego', 'Ok', 'Rock']
-      document.getElementById('console2' + classes[result2.label]);
-      // document.getElementById('console2').innerHTML = "Console2 prediction: " + classes[result2.label];
-
+      result2 = await classifier.predictClass(activation);
     } catch (error) {
-      console.log(error);
+      result2 = {};
     }
 
-    document.getElementById('console').innerHTML = 'Prediction: ' + result[0].className + " <Probability" + result[0].probability
+    const classes = ["Untrained", "Gato", "Dino" , "Persona", "OK","Rock"]
 
-    // limpiamos los tensores de la memoria, sin saturar el navegador
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+
+    try {
+      document.getElementById("console2").innerText = `
+    prediction: ${classes[result2.label]}\n
+    probability: ${result2.confidences[result2.label]}
+    `;
+    } catch (error) {
+      document.getElementById("console2").innerText="Untrained";
+    }
+
+
+
+    // Dispose the tensor to release the memory.
     img.dispose();
 
-    // esperamos al siguiente cuadro
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
     await tf.nextFrame();
+  }
+}
 
+img.onload =async function() {
+
+   try {
+     result = await net.classify(img);
+     descEl.innerHTML= JSON.stringify(result);
+   } catch (error) {
+
+   }
+ }
+
+async function  cambiarImagen(){
+  count =count + 1;
+  imgEl.src="https://picsum.photos/200/300?random=" + count;
+  descEl.innerHTM = "";
   }
 
-}
 
-imgEl.onload = async function () {
-  displayImagePredicction();
-}
-
-async function addExample(classId) {
-
-  console.log('add example');
-
+//add example
+async function addExample (classId) {
   const img = await webcam.capture();
   const activation = net.infer(img, true);
-
-  // asemos el entrenamiento
-  classiFier.addExample(activation, classId);
-
-  // limpiamos la memoria
-  img.dispose();
+  classifier.addExample(activation, classId);
+  //liberamos el tensor
+  img.dispose()
 }
 
-async function displayImagePredicction() {
-  try {
-    result = await net.classify(imgEl);
-    desEl.innerHTML = JSON.stringify(result);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// const saveKnn = async () => {
+//     //obtenemos el dataset actual del clasificador (labels y vectores)
+//     let strClassifier = JSON.stringify(Object.entries(classifier.getClassifierDataset()).map(([label, data]) => [label, Array.from(data.dataSync()), data.shape]));
+//     const storageKey = "knnClassifier";
+//     //lo almacenamos en el localStorage
+//     localStorage.setItem(storageKey, strClassifier);
+// };
 
-var count = 0;
-async function cambiarImg() {
-  count = count + 1;
-  imgEl.src = 'https://picsum.photos/200/300?random=' + count;
-}
 
-app();
+// const loadKnn = async ()=>{
+//     const storageKey = "knnClassifier";
+//     let datasetJson = localStorage.getItem(storageKey);
+//     classifier.setClassifierDataset(Object.fromEntries(JSON.parse(datasetJson).map(([label, data, shape]) => [label, tf.tensor(data, shape)])));
+// };
+
+
+app()
+
+
+// version 1
+// let net;
+
+// const imgEl = document.getElementById('img');
+// const desEl = document.getElementById('descripcion_img');
+// const webcamElement = document.getElementById('webcamp');
+// const classiFier = knnClassifier.create();
+
+// async function app() {
+//   net = await mobilenet.load();
+
+//   var result = await net.classify(imgEl);
+
+//   console.log(result);
+//   displayImagePredicction();
+
+//   // para usar y ver la camara WebCamp
+//   webcam = await tf.data.webcam(webcamElement);
+
+//   while (true) {
+//     const img = await webcam.capture();
+
+//     const result = await net.classify(img);
+
+//     const activation = net.infer(img, "conv_preds");
+//     var result2;
+
+//     try {
+//       result2 = await classiFier.predicClass(activation);
+//       const classes = ['Untrained', 'Gato', 'Dino', 'Diego', 'Ok', 'Rock']
+//       // document.getElementById('console2' + classes[result2.label]);
+//       document.getElementById('console2').innerHTML = "Console2 prediction: " + classes[result2.label];
+
+//     } catch (error) {
+//       console.log(error);
+//     }
+
+//     document.getElementById('console').innerHTML = 'Prediction: ' + result[0].className + " <Probability" + result[0].probability
+
+//     // limpiamos los tensores de la memoria, sin saturar el navegador
+//     img.dispose();
+
+//     // esperamos al siguiente cuadro
+//     await tf.nextFrame();
+
+//   }
+
+// }
+
+// imgEl.onload = async function () {
+//   displayImagePredicction();
+// }
+
+// async function addExample(classId) {
+
+//   console.log('add example');
+
+//   const img = await webcam.capture();
+//   const activation = net.infer(img, true);
+
+//   // asemos el entrenamiento
+//   classiFier.addExample(activation, classId);
+
+//   // limpiamos la memoria
+//   img.dispose();
+// }
+
+// async function displayImagePredicction() {
+//   try {
+//     result = await net.classify(imgEl);
+//     desEl.innerHTML = JSON.stringify(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// var count = 0;
+// async function cambiarImg() {
+//   count = count + 1;
+//   imgEl.src = 'https://picsum.photos/200/300?random=' + count;
+// }
+
+// app();
