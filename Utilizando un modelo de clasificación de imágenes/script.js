51fab97088fbee5052c563e63e9980e81d9371ad
Diego Luna <diegofcolunalopez@gmail.com>
2020-09-09 14:46:06 -0500

    Transferencia de conocimiento a un modelo de clasificacion de imagenes

--- Utilizando un modelo de clasificación de imágenes/script.js
+++ Utilizando un modelo de clasificación de imágenes/script.js
@@ -2,6 +2,8 @@ let net;
 
 const imgEl = document.getElementById('img');
 const desEl = document.getElementById('descripcion_img');
+const webcamElement = document.getElementById('webcamp');
+const classiFier = knnClassifier.create();
 
 async function app() {
   net = await mobilenet.load();
@@ -10,10 +12,56 @@ async function app() {
 
   console.log(result);
   displayImagePredicction();
+
+  // para usar y ver la camara WebCamp
+  webcam = await tf.data.webcam(webcamElement);
+
+  while (true) {
+    const img = await webcam.capture();
+
+    const result = await net.classify(img);
+
+    const activation = net.infer(img, "conv_preds");
+    var result2;
+
+    try {
+      result2 = await classiFier.predicClass(activation);
+      const classes = ['Untrained', 'Gato', 'Dino', 'Alex', 'Ok', 'Rock']
+      // document.getElementById('console2' + classes[result2.label]);
+      document.getElementById('console2').innerHTML = "Console2 prediction: " + classes[result2.label];
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    document.getElementById('console').innerHTML = 'Prediction: ' + result[0].className + " <Probability" + result[0].probability
+
+    // limpiamos los tensores de la memoria, sin saturar el navegador
+    img.dispose();
+
+    // esperamos al siguiente cuadro
+    await tf.nextFrame();
+
+  }
+
 }
 
 imgEl.onload = async function () {
   displayImagePredicction();
+}
+
+async function addExample(classId) {
+
+  console.log('add example');
+
+  const img = await webcam.capture();
+  const activation = net.infer(img, true);
+
+  // asemos el entrenamiento
+  classiFier.addExample(activation, classId);
+
+  // limpiamos la memoria
+  img.dispose();
 }
 
 async function displayImagePredicction() {
